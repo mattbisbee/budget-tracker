b8094d3bf23a3e1a83afdd4fbc4ebab383963aa2
Biznaz <aldhelm7@gmail.com>
2021-09-06 14:36:51 -0700

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,67 @@
-//service worker file
+//Code structure is from module 19, lesson 4. We were instructed to look at this module/lesson for an overview on how to create a service worker.
+
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+//Files to cache in order to function offline
+const FILES_TO_CACHE = [
+  "./",
+  "./css/styles.css",
+  "./icons/icon-192x192.png",
+  "./js/idb.js",
+  "./js/index.js",
+  "./index.html"
+];
+
+// This tells the browser to wait until the work is complete before terminating the service worker
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+
+})
+
+// adds an event listener to the activate event
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// App fetches info from the cache
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
